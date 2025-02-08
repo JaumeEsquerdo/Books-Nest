a43f72ff5a4667246e4259f9cbb32f4a08c53f4e
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-02-08 23:06:09 +0100

    style cards

--- Book-nest/book-nest/vite.config.js
+++ Book-nest/book-nest/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/Books-Nest/', //nombre del repositorio para enlazarlo con git pages y que se muestre
 })
